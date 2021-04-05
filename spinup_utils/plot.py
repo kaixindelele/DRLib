2a5f71d4717b0504f12fcd3ea79584ffb9e8b778
kaixindelele <1985790413@qq.com>
2021-04-05 20:35:51 +0800

    修复了只plot一个随机种子的bug

--- spinup_utils/plot.py
+++ spinup_utils/plot.py
@@ -5,8 +5,6 @@
 3.固定好每个exp_name的颜色；
 4.可以调节曲线的线宽，便于观察；
 5.保存图片到本地，便于远程ssh画图~
-
-
 """
 
 import seaborn as sns
@@ -63,11 +61,9 @@ def plot_data(data, xaxis='Epoch', value="TestEpRet",
     """
     If you upgrade to any version of Seaborn greater than 0.8.1, switch from 
     tsplot to lineplot replacing L29 with:
-
         sns.lineplot(data=data, x=xaxis, y=value, hue=condition, ci='sd', **kwargs)
-
     Changes the colorscheme and the default legend style, though.        
-    
+
     plt.legend()
         loc:图例位置,可取('best', 'upper right', 'upper left', 'lower left', 'lower right', 
             'right', 'center left', 'center , right', 'lower center', 'upper center', 'center')
@@ -88,7 +84,6 @@ def plot_data(data, xaxis='Epoch', value="TestEpRet",
         bbox_to_anchor: (横向看右,纵向看下),如果要自定义图例位置或者将图例画在坐标外边,用它,
             比如bbox_to_anchor=(1.4,0.8),这个一般配合着ax.get_position(),
             set_position([box.x0, box.y0, box.width*0.8 , box.height])使用
-
     """
     plt.legend(loc='upper center',
                ncol=1,
@@ -99,7 +94,6 @@ def plot_data(data, xaxis='Epoch', value="TestEpRet",
     """
     For the version of the legend used in the Spinning Up benchmarking page, 
     swap L38 with:
-
     plt.legend(loc='upper center', ncol=6, handlelength=1,
                mode="expand", borderaxespad=0., prop={'size': 13})
     """
@@ -116,7 +110,6 @@ def get_datasets(logdir, condition=None):
     """
     Recursively look through logdir for output files produced by
     spinup.logx.Logger.
-
     Assumes that any file "progress.txt" is a valid hit.
     """
     global exp_idx
@@ -137,39 +130,45 @@ def get_datasets(logdir, condition=None):
             except Exception as e:
                 print("e:", e)
                 print('No file named config.json')
-    roots_names_dict = {exp_names[index]: roots[index] for index in range(len(exp_names))}
+    # just leave one seed:
+    # roots_names_dict = {exp_names[index]: roots[index] for index in range(len(exp_names))}
+    # exp_name(str) --> roots(list) with diff seeds
+    roots_names_dict = {exp_names[index]: roots for index in range(len(exp_names))}
     for key, value in roots_names_dict.items():
         print(key, value)
     # 按照实验名排序
     roots_names_list = sorted(roots_names_dict.items(), key=lambda x: x[0])
-    print(roots_names_list)
+    print("roots_names_list:", roots_names_list)
     roots_names_dict = {tup[0]: tup[1] for tup in roots_names_list}
+    print("roots_names_dict:", roots_names_dict)
 
-    print(roots_names_dict)
-    for exp_name, root in roots_names_dict.items():
-        condition1 = condition or exp_name or 'exp'
-        condition2 = condition1 + '-' + str(exp_idx)
-        exp_idx += 1
-        if condition1 not in units:
-            units[condition1] = 0
-        unit = units[condition1]
-        units[condition1] += 1
+    for exp_name, roots in roots_names_dict.items():
+        for root in roots:
+            condition1 = condition or exp_name or 'exp'
+            condition2 = condition1 + '-' + str(exp_idx)
+            exp_idx += 1
+            if condition1 not in units:
+                units[condition1] = 0
+            unit = units[condition1]
+            units[condition1] += 1
 
-        try:
-            exp_data = pd.read_table(os.path.join(root, 'progress.txt'))
-            line_num = len(exp_data)
-            print('line num:{}, read from {}'.format(line_num,
-                                                     os.path.join(root, 'progress.txt')))
-        except:
-            print('Could not read from %s' % os.path.join(root, 'progress.txt'))
-            continue
-        performance = 'TestSuccess' if 'TestSuccess' in exp_data else 'AverageEpRet'
-        exp_data.insert(len(exp_data.columns), 'Unit', unit)
-        exp_data.insert(len(exp_data.columns), 'Condition1', condition1)
-        exp_data.insert(len(exp_data.columns), 'Condition2', condition2)
-        exp_data.insert(len(exp_data.columns), 'Performance', exp_data[performance])
-        datasets.append(exp_data)
+            try:
+                exp_data = pd.read_table(os.path.join(root, 'progress.txt'))
+                line_num = len(exp_data)
+                print('line num:{}, read from {}'.format(line_num,
+                                                         os.path.join(root, 'progress.txt')))
+            except:
+                print('Could not read from %s' % os.path.join(root, 'progress.txt'))
+                continue
+            performance = 'TestSuccess' if 'TestSuccess' in exp_data else 'AverageEpRet'
+            exp_data.insert(len(exp_data.columns), 'Unit', unit)
+            exp_data.insert(len(exp_data.columns), 'Condition1', condition1)
+            exp_data.insert(len(exp_data.columns), 'Condition2', condition2)
+            exp_data.insert(len(exp_data.columns), 'Performance', exp_data[performance])
+            datasets.append(exp_data)
     # 默认按照时间顺序获取文件夹数据
+
+    # print("-"*10, 'sorted by time', '-'*10)
     # for root, _, files in os.walk(logdir):
     #     if 'progress.txt' in files:
     #         exp_name = None
@@ -212,7 +211,6 @@ def get_all_datasets(all_logdirs, legend=None, select=None, exclude=None):
     For every entry in all_logdirs,
         1) check if the entry is a real directory and if it is,
            pull data from it;
-
         2) if not, check to see if the entry is a prefix for a
            real directory, and pull data from that.
     """
@@ -236,16 +234,16 @@ def get_all_datasets(all_logdirs, legend=None, select=None, exclude=None):
     if select is not None:
         logdirs = [log for log in logdirs if all(x in log for x in select)]
     if exclude is not None:
-        logdirs = [log for log in logdirs if all(not(x in log) for x in exclude)]
+        logdirs = [log for log in logdirs if all(not (x in log) for x in exclude)]
 
     # Verify logdirs
-    print('Plotting from...\n' + '='*DIV_LINE_WIDTH + '\n')
+    print('Plotting from...\n' + '=' * DIV_LINE_WIDTH + '\n')
     for logdir in logdirs:
         print(logdir)
-    print('\n' + '='*DIV_LINE_WIDTH)
+    print('\n' + '=' * DIV_LINE_WIDTH)
 
     # Make sure the legend is compatible with the logdirs
-    assert not(legend) or (len(legend) == len(logdirs)), \
+    assert not (legend) or (len(legend) == len(logdirs)), \
         "Must give a legend title for each set of experiments."
 
     # Load data from logdirs
@@ -269,7 +267,7 @@ def make_plots(all_logdirs, legend=None,
     data = get_all_datasets(all_logdirs, legend, select, exclude)
     values = values if isinstance(values, list) else [values]
     condition = 'Condition2' if count else 'Condition1'
-    estimator = getattr(np, estimator)      # choose what to show on main curve: mean? max? min?
+    estimator = getattr(np, estimator)  # choose what to show on main curve: mean? max? min?
     for value in values:
         plt.figure()
         plot_data(data, xaxis=xaxis, value=value,
@@ -296,16 +294,16 @@ def main():
     if len(sys.argv) > 1:
         print("run in command")
         print("argv:", sys.argv)
-        print('-'*30)
+        print('-' * 30)
         parser.add_argument('logdir', nargs='*')
     else:
         print("run in pycharm")
         print('-' * 30)
         parser.add_argument('--logdir', '-r', type=list,
                             default=[
-                                     '/home/lyl/robot_code/DRLib/spinup_utils/HER_DRLib_rew_push_single_exps2/',
-                                     # '/home/lyl/robot_code/DRLib/spinup_utils/HER_DRLib_rew_push_fork_exps2/',
-                                     ])
+                                '/home/dongkun/spinup/DRLib/spinup_utils/HER_DRLib_rew_PP_fork_pos/2',
+                                # '/home/lyl/robot_code/DRLib/spinup_utils/HER_DRLib_rew_push_fork_exps2/',
+                            ])
     parser.add_argument('--legend', '-l', nargs='*')
     parser.add_argument('--xaxis', '-x', default='TotalEnvInteracts')
     parser.add_argument('--value', '-y', default='Performance', nargs='*')
@@ -314,17 +312,16 @@ def main():
     parser.add_argument('--smooth', '-s', type=int, default=20)
     parser.add_argument('--linewidth', '-lw', type=float, default=4)
     parser.add_argument('--select', nargs='*')
+    # parser.add_argument('--select', default=['pos10'], )
     parser.add_argument('--exclude', nargs='*')
     parser.add_argument('--est', default='mean')
     args = parser.parse_args()
     print(args)
     """
-
     Args: 
         logdir (strings): As many log directories (or prefixes to log 
             directories, which the plotter will autocomplete internally) as 
             you'd like to plot from.
-
         legend (strings): Optional way to specify legend for the plot. The 
             plotter legend will automatically use the ``exp_name`` from the
             config.json file, unless you tell it otherwise through this flag.
@@ -336,10 +333,8 @@ def main():
             legend string for each one of those matches---unless you have 
             removed some of them as candidates via selection or exclusion 
             rules (below).)
-
         xaxis (string): Pick what column from data is used for the x-axis.
              Defaults to ``TotalEnvInteracts``.
-
         value (strings): Pick what columns from data to graph on the y-axis. 
             Submitting multiple values will produce multiple graphs. Defaults
             to ``Performance``, which is not an actual output of any algorithm.
@@ -349,22 +344,17 @@ def main():
             off-policy algorithms. The plotter will automatically figure out 
             which of ``AverageEpRet`` or ``AverageTestEpRet`` to report for 
             each separate logdir.
-
         count: Optional flag. By default, the plotter shows y-values which
             are averaged across all results that share an ``exp_name``, 
             which is typically a set of identical experiments that only vary
             in random seed. But if you'd like to see all of those curves 
             separately, use the ``--count`` flag.
-
         smooth (int): Smooth data by averaging it over a fixed window. This 
             parameter says how wide the averaging window will be.
-
         select (strings): Optional selection rule: the plotter will only show
             curves from logdirs that contain all of these substrings.
-
         exclude (strings): Optional exclusion rule: plotter will only show 
             curves from logdirs that do not contain these substrings.
-
     """
 
     make_plots(args.logdir, args.legend, args.xaxis, args.value, args.count,
@@ -375,3 +365,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
