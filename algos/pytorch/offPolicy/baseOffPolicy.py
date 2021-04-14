84a6e3dbe97a9856d652e3d2a4fb38e41b653c71
kaixindelele <1985790413@qq.com>
2021-04-14 19:04:54 +0800

    增加保存replay buffer的功能，可以实现断点重启的效果

--- algos/pytorch/offPolicy/baseOffPolicy.py
+++ algos/pytorch/offPolicy/baseOffPolicy.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import copy
+import pickle
 from algos.pytorch.offPolicy.norm import StateNorm
 
 
@@ -223,6 +224,26 @@ class OffPolicy:
         act_save_path = save_path + '/actor.pth'
         torch.save(self.ac.state_dict(), act_save_path)
         print("save model to:", save_path)
+        
+        def save_replay_buffer(self, path):
+        """
+        Save the replay buffer as a pickle file.
+        path = 'dense_replay.pkl'
+
+        :param path: Path to the file where the replay buffer should be saved.
+            if path is a str or pathlib.Path, the path is automatically created if necessary.
+        """
+        with open(path, 'wb') as f:
+            pickle.dump(obj=self.replay_buffer, file=f,
+                        protocol=pickle.HIGHEST_PROTOCOL)
+
+    def load_replay_buffer(self, path):
+        """
+        Load a replay buffer from a pickle file.
+
+        :param path: Path to the pickled replay buffer.
+        """        
+        self.replay_buffer = pickle.load(open(path, 'rb'))
 
 
 if __name__ == '__main__':
