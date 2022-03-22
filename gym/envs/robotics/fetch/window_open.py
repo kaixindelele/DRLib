8c09030ded1307b7a94ec49cc7bb626d0dd51aa7
kaixindelele <1985790413@qq.com>
2022-03-22 15:50:26 +0800

    add
    add

---
+++ gym/envs/robotics/fetch/window_open.py
@@ -0,0 +1,23 @@
+import os
+from gym import utils
+from gym.envs.robotics import fetch_window_open_env
+
+
+# Ensure we get the path separator correct on windows
+MODEL_XML_PATH = os.path.join('fetch', 'window_open.xml')
+
+
+class FetchWindowOpenEnv(fetch_window_open_env.FetchEnv, utils.EzPickle):
+    def __init__(self, reward_type='sparse'):
+        initial_qpos = {
+            'robot0:slide0': 0.405,
+            'robot0:slide1': 0.48,
+            'robot0:slide2': 0.0,
+            # 'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+        }
+        fetch_window_open_env.FetchEnv.__init__(
+            self, MODEL_XML_PATH, has_object=False, block_gripper=False, n_substeps=20,
+            gripper_extra_height=0.1, target_in_the_air=False, target_offset=0.0,
+            obj_range=0.0, target_range=0.0, distance_threshold=0.02,
+            initial_qpos=initial_qpos, reward_type=reward_type)
+        utils.EzPickle.__init__(self)
